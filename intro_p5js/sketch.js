c9ae304d21f45cd1fe57fef7156ea5dbe2c93e30
aishaahmedabdullahi45 <91063890+aishaahmedabdullahi45@users.noreply.github.com>
2022-01-28 15:27:38 +0300

    Add files via upload

---
+++ intro_p5js/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,0,300);
+  //Drawing of ellipse shape
+  ellipse(250,140,100,50);
+  fill(2);
+  // Drawing of triangle
+  triangle(300,56,78,80,20,20,);
+  fill(260,10,10);
+}
